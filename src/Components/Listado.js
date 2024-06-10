84ddaf4743eb4e5962f33c28936817adafddfe93
Almiron Augusto <almironaugusto404@gmail.com>
2024-06-10 20:31:08 -0300

    hooks-content

--- src/Components/Listado.js
+++ src/Components/Listado.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
+
+  /** Estado para poner peliculas en el componente */
+
+  const [listadoState, setListadoState] = useState('');
+
+  /*Metodo Conseguir Peliculas */
+  const conseguirPeliculas = () => 
+  {
+    let peliculas = JSON.parse(localStorage.getItem('pelis'));
+    setListadoState(peliculas);
+
+    console.log(peliculas)
+
+  }
+
+  useEffect (()=>{
+    conseguirPeliculas();
+
+    console.log('Componente del listado de peliculas cargado!')
+  },[]);
   return (
     <>
             <article className="peli-item">
@@ -9,25 +29,7 @@ export const Listado = () => {
                 <button className="edit">Editar</button>
                 <button className="delete">Borrar</button>
             </article>
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">una pelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">una pelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">una pelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+            
     
     
     </>
